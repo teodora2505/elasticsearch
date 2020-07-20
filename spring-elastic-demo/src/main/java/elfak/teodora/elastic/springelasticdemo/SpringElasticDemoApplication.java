ab043eece2950a829816e78b182aff8f418e3b2d
teodora2505 <teodoranovkovic@hotmail.com>
2020-07-20 21:33:24 +0200

    upload inital project

---
+++ spring-elastic-demo/src/main/java/elfak/teodora/elastic/springelasticdemo/SpringElasticDemoApplication.java
@@ -0,0 +1,13 @@
+package elfak.teodora.elastic.springelasticdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringElasticDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringElasticDemoApplication.class, args);
+	}
+
+}
---
+++ spring-elastic-demo/src/test/java/elfak/teodora/elastic/springelasticdemo/SpringElasticDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package elfak.teodora.elastic.springelasticdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringElasticDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
